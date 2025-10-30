5c165a27f1c232f379e89ae74bbb1c760a974c24
eIIka <vkalkatins@gmail.com>
2025-10-30 13:19:28 +0200

    Create bug in method divide

--- src/main/java/ua/ellka/calculator/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/ua/ellka/calculator/calculator/service/CalculatorServiceImpl.java
@@ -23,7 +23,7 @@ public class CalculatorServiceImpl implements CalculatorService {
     @Override
     public int divide(int a, int b) {
         if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
+            return 0;
         }
         return a / b;
     }
